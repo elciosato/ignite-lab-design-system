aa185a03ad09f88a2fe5a9f28fd28769352cf0e4
Elcio Yoshiaki Sato <elciosato@gmail.com>
2022-10-12 12:23:47 +0200

    Add Text component in storybook

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './styles/global.css';
+
+export function App() {
+  return (
+    <div>
+      <h1 className='text-2xl'>Hello World</h1>
+      <button className='bg-cyan-500 rounded px-4 py-2 hover:bg-cyan-300'>Submit</button>
+    </div>
+  )
+}
+
